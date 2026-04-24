7df1f3074377c97cc65b5d32c0a96355b5e77088
Victor <el.rachman@unesp.br>
2026-04-24 08:30:02 -0300

    grafico

--- js-graficos.js
+++ js-graficos.js
@@ -2,25 +2,69 @@ async function verificar() {
     const log = await fetch("http://127.0.0.1:5000/status", {
         credentials: "include"
     });
+
     const log_json = await log.json()
-    if(!log_json.logado)
-    {
+
+    if(!log_json.logado) {
         window.location.href = "http://127.0.0.1:5500/index.html"
     }
 }
 
-verificar()
+function criarGraf(dias, tempo) {
+    const ctx = document.getElementById('grap').getContext('2d')
 
-async function tratarDados()
-{
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: dias,
+            datasets: [{
+                label: "Tempo de estudo Matemática",
+                data: tempo,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false,  
+            maintainAspectRatio: false,
+            scales: {
+                y:{
+                    ticks:{
+                        callback: function (value){
+                            return value + " min"
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+async function tratarDados() {
     const res = await fetch("http://127.0.0.1:5000/mostra-dados", {
         credentials: "include"
     });
 
     const dados = await res.json()
 
-    const p = document.getElementById('gra')
-    p.innerText = JSON.stringify(dados)
+    console.log(dados)
+
+    const mat = dados.mat || []
+    const dias_m = mat.map(item => {
+        const d = new Date(item.dia)
+        return d.toLocaleDateString('pt-br')
+    })
+    const tempo_m = mat.map(item => item.tempo)
+
+    console.log(dias_m, tempo_m)
+
+    criarGraf(dias_m, tempo_m)
+
+    document.getElementById('gra').innerText = JSON.stringify(dados)
 }
 
-tratarDados()
+async function iniciar() {
+    await verificar()
+    await tratarDados()
+}
+
+iniciar()
